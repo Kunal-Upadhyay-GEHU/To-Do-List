2b73da65f482d6bd21d40bc2c1c69ac5cd7ee42c
Kunal-Upadhyay-GEHU <149419637+Kunal-Upadhyay-GEHU@users.noreply.github.com>
2023-10-30 23:41:02 +0530

    update

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const todoList = document.getElementById('todo-list');
+    const todoInput = document.getElementById('todo-input');
+    const addButton = document.getElementById('add-button');
+    const container = document.querySelector('.container');
+
+
+    addButton.addEventListener('click', function () {
+        const task = todoInput.value;
+        if (task.trim() !== '') {
+            addTodoItem(task);
+            todoInput.value = '';
+        }
+    });
+
+    todoInput.addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') {
+            const task = todoInput.value;
+            if (task.trim() !== '') {
+                addTodoItem(task);
+                todoInput.value = '';
+            }
+        }
+    });
+
+    todoList.addEventListener('click', function (event) {
+        if (event.target.classList.contains('delete-button')) {
+            event.target.parentElement.remove();
+        }
+    });
+
+    function addTodoItem(task) {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span >${task}</span>
+            <button class="delete-button">
+  <svg class="delete-svgIcon" viewBox="0 0 448 512">
+                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+                  </svg>
+</button>
+        `;
+        todoList.appendChild(li);
+    }
+});
+
